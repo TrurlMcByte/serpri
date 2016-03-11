bea07efcedb1624183bd6f57cf58c052c5877e41
Trurl McByte <trurl@mcbyte.net>
2016-03-11 12:39:16 +0200

    fixes

--- example.php
+++ example.php
@@ -3,13 +3,13 @@
 
 include 'serpri.php';
 
-$s = array(5.5, false, 100, null, 'roger' => 500, 'test', func => function () {echo 'Ok';});
+$s = array(5.5, false, 100, null, 'roger' => 500, 'test', 'func' => function () {echo 'Ok';});
 $s['rec'] = &$s;
 $a = new stdClass();
 $s['b'] = new stdClass();
 $s['c'] = new stdClass();
 $a->sub = new stdClass();
-$a->b = $b;
+$a->b = $s['b'];
 $s['b'] = $a;$s['c'] = $a;
 
 $p = new serpri($s);
